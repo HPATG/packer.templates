d62faa87811f93e3a36673799844698471821bb5
Yazz Atlas <yazz.atlas@hp.com>
2015-03-09 14:05:17 -0700

    Removed some debugging

--- script/common/parallels.sh
+++ script/common/parallels.sh
@@ -3,20 +3,8 @@
 #if [ $PACKER_BUILDER_TYPE = parallels*  ] 
 apt-get install -y linux-headers-$(uname -r) build-essential
 
-echo ################################
-echo ################################
-echo ################################
-echo ################################
-echo ################################
-
 mount -r -t iso9660 /dev/cdrom /mnt
 /mnt/install --install-unattended --progress
-
-echo ################################
-echo ################################
-echo ################################
-echo ################################
-echo ################################
 
 cat /var/log/parallels-tools-install.log
 
